c7652dec94339e1c3591ac84c4762494d0c25511
gluten5659 <140404099+gluten5659@users.noreply.github.com>
2026-06-23 13:06:29 +0500

    ref: iterrarion method

--- internal/compare/compare.go
+++ internal/compare/compare.go
@@ -22,76 +22,62 @@ type Diff struct {
 }
 
 func Compare(firstFile, secondFile map[string]any) []Diff {
-	var diff []Diff
+	keys := sortedKeys(firstFile, secondFile)
 
-	for _, key := range sortedKeys(firstFile, secondFile) {
-		firstFileValue, isKeyInFirstFile := firstFile[key]
-		secondFileValue, isKeyInSecondFile := secondFile[key]
-		firstFileValueMap, isFirstValueMap := firstFile[key].(map[string]any)
-		secondFileValueMap, isSecondValueMap := secondFile[key].(map[string]any)
-
-		if isFirstValueMap && isSecondValueMap {
-			diff = append(
-				diff,
-				Diff{
-					Change: NoChanges,
-					Key:    key,
-					Value:  Compare(firstFileValueMap, secondFileValueMap),
-				},
-			)
-
-			continue
-		}
-
-		if isFirstValueMap {
-			diff = append(
-				diff,
-				Diff{
-					Change: Deleted,
-					Key:    key,
-					Value:  Compare(firstFileValueMap, firstFileValueMap),
-				},
-			)
-
-			continue
-		}
-
-		if isSecondValueMap {
-			diff = append(
-				diff,
-				Diff{
-					Change: Added,
-					Key:    key,
-					Value:  Compare(secondFileValueMap, secondFileValueMap),
-				},
-			)
-
-			continue
-		}
-
-		if !isKeyInFirstFile {
-			diff = append(diff, Diff{Change: Added, Key: key, Value: secondFile[key]})
-
-			continue
-		}
-
-		if !isKeyInSecondFile {
-			diff = append(diff, Diff{Change: Deleted, Key: key, Value: firstFileValue})
-
-			continue
-		}
-
-		if !reflect.DeepEqual(firstFileValue, secondFileValue) {
-			diff = append(diff, Diff{Change: Deleted, Key: key, Value: firstFileValue})
-			diff = append(diff, Diff{Change: Added, Key: key, Value: secondFileValue})
-
-			continue
-		}
-
-		diff = append(diff, Diff{Change: NoChanges, Key: key, Value: firstFileValue})
+	diff := make([]Diff, 0, len(keys))
+	for _, key := range keys {
+		diff = append(diff, compareKey(key, firstFile, secondFile)...)
 	}
 
 	return diff
+}
+
+func compareKey(key string, firstFile, secondFile map[string]any) []Diff {
+	firstFileValue, isKeyInFirstFile := firstFile[key]
+	secondFileValue, isKeyInSecondFile := secondFile[key]
+	firstFileValueMap, isFirstValueMap := firstFile[key].(map[string]any)
+	secondFileValueMap, isSecondValueMap := secondFile[key].(map[string]any)
+
+	if isFirstValueMap && isSecondValueMap {
+		return []Diff{{
+			Change: NoChanges,
+			Key:    key,
+			Value:  Compare(firstFileValueMap, secondFileValueMap),
+		}}
+	}
+
+	if isFirstValueMap {
+		return []Diff{{
+			Change: Deleted,
+			Key:    key,
+			Value:  Compare(firstFileValueMap, firstFileValueMap),
+		}}
+	}
+
+	if isSecondValueMap {
+		return []Diff{{
+			Change: Added,
+			Key:    key,
+			Value:  Compare(secondFileValueMap, secondFileValueMap),
+		}}
+	}
+
+	if !isKeyInFirstFile {
+		return []Diff{{Change: Added, Key: key, Value: secondFile[key]}}
+	}
+
+	if !isKeyInSecondFile {
+		return []Diff{{Change: Deleted, Key: key, Value: firstFileValue}}
+	}
+
+	if !reflect.DeepEqual(firstFileValue, secondFileValue) {
+		return []Diff{
+			{Change: Deleted, Key: key, Value: firstFileValue},
+			{Change: Added, Key: key, Value: secondFileValue},
+		}
+	}
+
+	return []Diff{{Change: NoChanges, Key: key, Value: firstFileValue}}
 }
 
 func sortedKeys[Key cmp.Ordered, Value any](sources ...map[Key]Value) []Key {
