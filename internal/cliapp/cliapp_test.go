bcac8a4b03fb112f1f65ca79ae1b9ea7999d114f
gluten5659 <140404099+gluten5659@users.noreply.github.com>
2026-07-13 16:40:44 +0500

    test: cleapp test refactor

--- internal/cliapp/cliapp_test.go
+++ internal/cliapp/cliapp_test.go
@@ -37,277 +37,63 @@ func newTestCommand(tb testing.TB, output *strings.Builder) *cli.Command {
 	return command
 }
 
-const flatStylishDiff = "{\n" +
-	"  - follow: false\n" +
-	"    host: hexlet.io\n" +
-	"  - proxy: 123.234.53.22\n" +
-	"  - timeout: 50\n" +
-	"  + timeout: 20\n" +
-	"  + verbose: true\n" +
-	"}\n"
+func readGolden(tb testing.TB, path string) string {
+	tb.Helper()
 
-func TestCommandRendersDiff(t *testing.T) {
-	t.Parallel()
+	content, err := os.ReadFile(path)
+	require.NoError(tb, err)
 
-	output := strings.Builder{}
-	command := newTestCommand(t, &output)
-
-	arguments := []string{commandName, "testdata/file1.json", "testdata/file2.json"}
-
-	err := command.Run(t.Context(), arguments)
-
-	require.NoError(t, err)
-
-	assert.Equal(t, flatStylishDiff, output.String())
+	return string(content)
 }
 
-func TestCommandRendersYAMLDiff(t *testing.T) {
+func TestCommandRendersGolden(t *testing.T) {
 	t.Parallel()
 
-	output := strings.Builder{}
-	command := newTestCommand(t, &output)
-
-	arguments := []string{commandName, "testdata/file1.yaml", "testdata/file2.yaml"}
-
-	err := command.Run(t.Context(), arguments)
-
-	require.NoError(t, err)
-
-	assert.Equal(t, flatStylishDiff, output.String())
-}
-
-func TestCommandRendersNestedDiff(t *testing.T) {
-	t.Parallel()
-
-	output := strings.Builder{}
-	command := newTestCommand(t, &output)
-
-	arguments := []string{commandName, nestedFirst, nestedSecond}
-
-	err := command.Run(t.Context(), arguments)
-
-	require.NoError(t, err)
-
-	expected := "{\n" +
-		"    common: {\n" +
-		"      + follow: false\n" +
-		"        setting1: Value 1\n" +
-		"      - setting2: 200\n" +
-		"      - setting3: true\n" +
-		"      + setting3: null\n" +
-		"      + setting4: blah blah\n" +
-		"      + setting5: {\n" +
-		"            key5: value5\n" +
-		"        }\n" +
-		"        setting6: {\n" +
-		"            doge: {\n" +
-		"              - wow: \n" +
-		"              + wow: so much\n" +
-		"            }\n" +
-		"            key: value\n" +
-		"          + ops: vops\n" +
-		"        }\n" +
-		"    }\n" +
-		"    group1: {\n" +
-		"      - baz: bas\n" +
-		"      + baz: bars\n" +
-		"        foo: bar\n" +
-		"      - nest: {\n" +
-		"            key: value\n" +
-		"        }\n" +
-		"      + nest: str\n" +
-		"    }\n" +
-		"  - group2: {\n" +
-		"        abc: 12345\n" +
-		"        deep: {\n" +
-		"            id: 45\n" +
-		"        }\n" +
-		"    }\n" +
-		"  + group3: {\n" +
-		"        deep: {\n" +
-		"            id: {\n" +
-		"                number: 45\n" +
-		"            }\n" +
-		"        }\n" +
-		"        fee: 100500\n" +
-		"    }\n" +
-		"}\n"
-
-	assert.Equal(t, expected, output.String())
-}
-
-func TestCommandRendersPlainDiff(t *testing.T) {
-	t.Parallel()
-
-	output := strings.Builder{}
-	command := newTestCommand(t, &output)
-
-	arguments := []string{
-		commandName,
-		formatFlag,
-		"plain",
-		nestedFirst,
-		nestedSecond,
+	testCases := []struct {
+		name       string
+		arguments  []string
+		goldenFile string
+	}{
+		{
+			name:       "flat json inputs render in stylish",
+			arguments:  []string{commandName, "testdata/file1.json", "testdata/file2.json"},
+			goldenFile: "testdata/expected/flat.stylish",
+		},
+		{
+			name:       "flat yaml inputs render the same stylish output",
+			arguments:  []string{commandName, "testdata/file1.yaml", "testdata/file2.yaml"},
+			goldenFile: "testdata/expected/flat.stylish",
+		},
+		{
+			name:       "nested inputs render in stylish",
+			arguments:  []string{commandName, nestedFirst, nestedSecond},
+			goldenFile: "testdata/expected/nested.stylish",
+		},
+		{
+			name:       "nested inputs render in plain",
+			arguments:  []string{commandName, formatFlag, "plain", nestedFirst, nestedSecond},
+			goldenFile: "testdata/expected/nested.plain",
+		},
+		{
+			name:       "nested inputs render in json",
+			arguments:  []string{commandName, formatFlag, "json", nestedFirst, nestedSecond},
+			goldenFile: "testdata/expected/nested.json",
+		},
 	}
 
-	err := command.Run(t.Context(), arguments)
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
 
-	require.NoError(t, err)
+			output := strings.Builder{}
+			command := newTestCommand(t, &output)
 
-	expected := "Property 'common.follow' was added with value: false\n" +
-		"Property 'common.setting2' was removed\n" +
-		"Property 'common.setting3' was updated. From true to null\n" +
-		"Property 'common.setting4' was added with value: 'blah blah'\n" +
-		"Property 'common.setting5' was added with value: [complex value]\n" +
-		"Property 'common.setting6.doge.wow' was updated. From '' to 'so much'\n" +
-		"Property 'common.setting6.ops' was added with value: 'vops'\n" +
-		"Property 'group1.baz' was updated. From 'bas' to 'bars'\n" +
-		"Property 'group1.nest' was updated. From [complex value] to 'str'\n" +
-		"Property 'group2' was removed\n" +
-		"Property 'group3' was added with value: [complex value]\n"
+			err := command.Run(t.Context(), testCase.arguments)
 
-	assert.Equal(t, expected, output.String())
-}
-
-func TestCommandRendersJSONDiff(t *testing.T) {
-	t.Parallel()
-
-	output := strings.Builder{}
-	command := newTestCommand(t, &output)
-
-	arguments := []string{
-		commandName,
-		formatFlag,
-		"json",
-		nestedFirst,
-		nestedSecond,
+			require.NoError(t, err)
+			assert.Equal(t, readGolden(t, testCase.goldenFile), output.String())
+		})
 	}
-
-	err := command.Run(t.Context(), arguments)
-
-	require.NoError(t, err)
-
-	expected := `{
-  "diff": [
-    {
-      "children": [
-        {
-          "key": "follow",
-          "type": "added",
-          "value": false
-        },
-        {
-          "key": "setting1",
-          "type": "unchanged",
-          "value": "Value 1"
-        },
-        {
-          "key": "setting2",
-          "type": "removed",
-          "value": 200
-        },
-        {
-          "key": "setting3",
-          "newValue": null,
-          "oldValue": true,
-          "type": "updated"
-        },
-        {
-          "key": "setting4",
-          "type": "added",
-          "value": "blah blah"
-        },
-        {
-          "key": "setting5",
-          "type": "added",
-          "value": {
-            "key5": "value5"
-          }
-        },
-        {
-          "children": [
-            {
-              "children": [
-                {
-                  "key": "wow",
-                  "newValue": "so much",
-                  "oldValue": "",
-                  "type": "updated"
-                }
-              ],
-              "key": "doge",
-              "type": "nested"
-            },
-            {
-              "key": "key",
-              "type": "unchanged",
-              "value": "value"
-            },
-            {
-              "key": "ops",
-              "type": "added",
-              "value": "vops"
-            }
-          ],
-          "key": "setting6",
-          "type": "nested"
-        }
-      ],
-      "key": "common",
-      "type": "nested"
-    },
-    {
-      "children": [
-        {
-          "key": "baz",
-          "newValue": "bars",
-          "oldValue": "bas",
-          "type": "updated"
-        },
-        {
-          "key": "foo",
-          "type": "unchanged",
-          "value": "bar"
-        },
-        {
-          "key": "nest",
-          "newValue": "str",
-          "oldValue": {
-            "key": "value"
-          },
-          "type": "updated"
-        }
-      ],
-      "key": "group1",
-      "type": "nested"
-    },
-    {
-      "key": "group2",
-      "type": "removed",
-      "value": {
-        "abc": 12345,
-        "deep": {
-          "id": 45
-        }
-      }
-    },
-    {
-      "key": "group3",
-      "type": "added",
-      "value": {
-        "deep": {
-          "id": {
-            "number": 45
-          }
-        },
-        "fee": 100500
-      }
-    }
-  ]
-}
-`
-
-	assert.Equal(t, expected, output.String())
 }
 
 func TestCommandExitCodes(t *testing.T) {
